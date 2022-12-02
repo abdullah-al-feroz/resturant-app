0690249bbcbc4b82382ba53800ab32f21b0dfe4d
Md. Abdullah- Al Firoz <firozcse2020@gmail.com>
2022-12-03 01:42:55 +0600

    Animated header successfully added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Logo from './../img/logo.png'
 import { MdShoppingBasket } from 'react-icons/md'
+import {motion} from 'framer-motion'
 import Avatar from './../img/avatar.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header className='fixed z-50 w-screen p-6 px-16'>
       {/* desktop & tablet */}
       <div className='hidden md:flex w-full h-full items-center justify-between'>
-        <div className='flex items-center gap-2'>
+        <Link className='flex items-center gap-2'>
           <img src={Logo} className='w-8 object-cover' alt='logo' />
           <p className='text-headingColor text-xl font-bold'> City</p>
-        </div>
+        </Link >
 
         <div className='flex items-center gap-8'>
         <ul className='flex items-center gap-8'>
@@ -31,7 +33,12 @@ const Header = () => {
           </div>
         </div>
 
-        <img src={Avatar} className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-*l ' alt='userprofile'/>
+        <motion.img 
+        whileTap={{scale: 0.6}}
+        src={Avatar} 
+        className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-*l cursor-pointer' 
+        alt='userprofile'
+        />
         </div>
       </div>
       {/* mobile */}
