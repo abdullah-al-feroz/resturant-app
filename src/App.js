df0cd3a04f21734c4176f8938b701e7d862b993c
Md. Abdullah- Al Firoz <firozcse2020@gmail.com>
2022-12-03 00:22:23 +0600

    Header Designed Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Header } from './components'
+import { Route, Routes } from 'react-router-dom'
+import { CreateContainer, Header, MainContainer } from './components'
+
 
 const App = () => {
   return (
-    <div className="w-screen h-auto flex flex-col">
+    <div className="w-screen h-auto flex flex-col bg-primary">
       <Header />
+
+      <main className='mt-24 p-8 w-full'>
+        <Routes>
+          <Route path='/*' element={<MainContainer/>}/>
+          <Route path='/createItem' element={<CreateContainer/>}/>
+        </Routes>
+      </main>
     </div>
   )
 
