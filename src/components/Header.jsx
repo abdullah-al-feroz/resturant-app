39fcb8884dbeef4a158a42f1207dc83bb4150e28
Md. Abdullah- Al Firoz <firozcse2020@gmail.com>
2022-12-21 01:27:25 +0600

    LocalStorage and Logout Added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Logo from './../img/logo.png'
-import { MdShoppingBasket } from 'react-icons/md'
+import { MdShoppingBasket, MdAdd, MdLogout } from 'react-icons/md'
 import { motion } from 'framer-motion'
 import Avatar from './../img/avatar.png'
 import { Link } from 'react-router-dom'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import {app} from './firebase.config'
+import { app } from './firebase.config'
 import { actionType } from '../context/reducer'
 import { useStateValue } from '../context/StateProvider'
 
@@ -13,14 +13,17 @@ const Header = () => {
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
-  const [{user}, dispatch] = useStateValue()
+  const [{ user }, dispatch] = useStateValue()
 
   const login = async () => {
-    const {user:{refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider);
-    dispatch({
-      type: actionType.SET_USER,
-      user: providerData[0]
-    })
+    if (!user) {
+      const { user: { refreshToken, providerData } } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0]
+      });
+      localStorage.setItem('user', JSON.stringify(providerData[0]))
+    }
   };
   return (
     <header className='fixed z-50 w-screen p-6 px-16'>
@@ -50,13 +53,18 @@ const Header = () => {
           </div>
 
           <div className='relative'>
-          <motion.img
-            whileTap={{ scale: 0.6 }}
-            src={Avatar}
-            className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-*l cursor-pointer'
-            alt='userprofile'
-            onClick={login}
-          />
+            <motion.img
+              whileTap={{ scale: 0.6 }}
+              src={user ? user.photoURL : Avatar}
+              className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-*l cursor-pointer rounded-full'
+              alt='userprofile'
+              onClick={login}
+            />
+          </div>
+          <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 px-4 py-2'>
+            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>New Item <MdAdd/></p>
+            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>Logout <MdLogout/></p>
+
           </div>
         </div>
       </div>
