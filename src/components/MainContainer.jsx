7c8de3d925aa83f32257bf1da103cc9534c370ce
Md. Abdullah- Al Firoz <95273951+abdullah-al-feroz@users.noreply.github.com>
2023-02-24 02:15:08 +0600

    Home Container Implemented Successfully

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { useStateValue } from "../context/StateProvider";
+import HomeContainer from "./HomeContainer";
 
 const MainContainer = () => {
 
@@ -15,6 +16,7 @@ const MainContainer = () => {
 
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center ">
+      <HomeContainer />
       <section className="w-full my-6">
         <div className="w-full flex items-center justify-between">
           <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
