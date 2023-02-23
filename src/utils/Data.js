81abc65aed6628dd2ce22043d8fc28a52685534e
Md. Abdullah- Al Firoz <95273951+abdullah-al-feroz@users.noreply.github.com>
2023-02-24 02:45:36 +0600

    Create Container Implemented
    --Create Container implemented
    --Loader Implemented
    --Cart Item Implemented

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -4,32 +4,32 @@ import C3 from "../img/c3.png";
 import Fi1 from "../img/fi1.png";
 
 export const heroData = [
-  {
-    id: 1,
-    name: "Icecream",
-    decp: "Chocolate & vanilla",
-    price: "5.25",
-    imageSrc: I1,
-  },
-  {
-    id: 2,
-    name: "Strawberries",
-    decp: "Fresh Strawberries",
-    price: "10.25",
-    imageSrc: F1,
-  },
-  {
-    id: 3,
-    name: "Chicken Kebab",
-    decp: "Mixed Kebab Plate",
-    price: "8.25",
-    imageSrc: C3,
-  },
-  {
-    id: 4,
-    name: "Fish Kebab",
-    decp: "Mixed Fish Kebab",
-    price: "5.25",
-    imageSrc: Fi1,
-  },
+    {
+        id: 1,
+        name: "Icecream",
+        decp: "Chocolate & vanilla",
+        price: "5.25",
+        imageSrc: I1,
+    },
+    {
+        id: 2,
+        name: "Strawberries",
+        decp: "Fresh Strawberries",
+        price: "10.25",
+        imageSrc: F1,
+    },
+    {
+        id: 3,
+        name: "Chicken Kebab",
+        decp: "Mixed Kebab Plate",
+        price: "8.25",
+        imageSrc: C3,
+    },
+    {
+        id: 4,
+        name: "Fish Kebab",
+        decp: "Mixed Fish Kebab",
+        price: "5.25",
+        imageSrc: Fi1,
+    },
 ];
